9a85c0fe6ced44dc84df47bde9c29a2afbece525
yasmeinelkenany <yasmeinelkenany@gmail.com>
2025-10-11 18:34:37 +0300

    add: qoutes array, delete: api urls file

---
+++ utils/qoutes.js
@@ -0,0 +1,8 @@
+export const qoutesArray = [
+    "GC=F", "CL=F", "SI=F", "NG=F", "ZS=F", "ZW=F",
+    "EURUSD=X", "JPY=X", "GBPUSD=X", "TRY=X", "CHF=X", "CAD=X",
+    "BTC-USD", "ETH-USD", "USDT-USD", "XRP-USD", "SOL-USD", "DOT-USD",
+    "SPY", "QQQ", "TLT", "EWZ", "SQQQ", "TQQQ",
+    "VFIAX", "FXAIX", "SWPPX", "TRBCX", "AGTHX", "DODGX",
+    "^FVX", "^TNX", "^TYX", "^IRX"
+]
